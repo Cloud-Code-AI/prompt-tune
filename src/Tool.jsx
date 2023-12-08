ed8b51f97a5d0cfea4458b29b3430f4ef2e8d99d
Shreyash Gupta <sg973@cornell.edu>
2023-12-08 14:08:01 +0530

    prompt update logic

--- src/Tool.jsx
+++ src/Tool.jsx
@@ -17,43 +17,90 @@ import {
 import { Button } from "./components/ui/button";
 
 const promptData = [
-    {
-        number: "1",
-        prompt: "Sample prompt 1"
-    },
-    {
-        number: "2",
-        prompt: "Sample prompt 2"
-    },
-    {
-        number: "3",
-        prompt: "Sample prompt 3"
-    },
-    {
-        number: "4",
-        prompt: "Sample prompt 4"
-    },
-    {
-        number: "5",
-        prompt: "Sample prompt 5"
-    }
+    "Sample prompt 1",
+    "Sample prompt 2",
+    "Sample prompt 3",
+    "Sample prompt 4",
+    "Sample prompt 5"
 ]
 
 export default function Tool () {
 
     const [loading, setLoading] = useState(false)
+    const [chatLoading, setChatLoading] = useState(false)
     const [messages, setMessages] = useState([
         { text: "Hey there! How may I assist you?", sender: "Bot" },
       ]);
-
-    const submitPrompt = (e, source) => {
+    
+    const [option1, setOption1] = useState({
+        text: "Enter prompt to generate options",
+        id: -1
+    })
+    const [option2, setOption2] = useState({
+        text: "Enter prompt to generate options",
+        id: 0
+    })
+    const submitPrompt = (e) => {
         e.preventDefault();
-        console.log(source)
+        setLoading(true)
+        //TODO: Add API call here to load 5 prompt options.
+        const resp = "response"
+        console.log(resp)
+        setOption1({
+            text: promptData[0],
+            id: 0
+        })
+        setOption2({
+            text: promptData[1],
+            id: 1
+        })
+        setLoading(false)
     }
 
-    const submitChatPrompt = (e, source) => {
+    const submitChatPrompt = (e,) => {
         e.preventDefault();
-        console.log(source)
+        setChatLoading(true) 
+    }
+
+    const handleSelect = (id) => {
+        console.log(id)
+        console.log(option1.id)
+        console.log(option2.id)
+        console.log(option1.id === option2.id)
+        if (id === 1) {
+            if (option2.id < 5) {
+                if (option2.id + 1 != option1.id && option2.id + 1 < 5) {
+                    setOption2({
+                        text: promptData[option2.id + 1],
+                        id: option2.id + 1
+                    })
+                }
+                else if (option2.id + 2 != option1.id && option2.id + 2 < 5) {
+                    setOption2({
+                        text: promptData[option2.id + 2],
+                        id: option2.id + 2
+                    })
+                }
+            }
+            
+        }
+        if (id === 2) {
+            if (option1.id < 5) {
+                if (option1.id + 1 != option2.id && option1.id + 1 < 5) {
+                    setOption1({
+                        text: promptData[option1.id + 1],
+                        id: option1.id + 1
+                    })
+                }
+                else if (option1.id + 2 != option2.id && option1.id + 2 < 5) {
+                    setOption1({
+                        text: promptData[option1.id + 2],
+                        id: option1.id + 2
+                    })
+                }
+            }
+            
+        }
     }
 
     return (
@@ -63,7 +110,7 @@ export default function Tool () {
             <div className="border rounded-md h-full">
                 <div className="w-full h-[18%] py-4 border-b">
                     <div className="flex flex-col gap-y-2 w-full">
-                        <Label className="px-8 pt-2 text-xs text-gray-600">PROMPT</Label>
+                        <Label className="px-8 pt-2 text-xs text-primary/50">PROMPT</Label>
                         <form id="promptForm" className="px-4 h-full w-full">
                             <label htmlFor="prompt" className="sr-only">Enter your prompt</label>
                             <div className="relative h-full">
@@ -76,13 +123,13 @@ export default function Tool () {
                                     onKeyDown={(e) => {
                                         if (e.key === 'Enter' && !e.shiftKey) {
                                             e.preventDefault();
-                                            submitPrompt(e, "desktop");
+                                            submitPrompt(e);
                                         }
                                     }}
                                 />
                                 <button
                                     type="submit"
-                                    onClick={(e) => submitPrompt(e, "desktop")}
+                                    onClick={(e) => submitPrompt(e)}
                                     className="absolute md:top-1 top-0 right-2.5 rounded-lg p-2"
                                 >
                                     {loading ? <ThreeDots fill="#1e293b" speed={0.75} className='rounded-r-md h-5 w-5' /> :
@@ -95,29 +142,29 @@ export default function Tool () {
                 </div>
                 <div className="flex h-[82%] w-full">
                     <div className="flex flex-col w-full h-full">
-                        <p className="px-6 pt-6 text-gray-600">Please choose one of the two options</p>
+                        <p className="px-6 pt-6 text-primary/50">Please choose one of the two options</p>
                         <div className="w-full h-full p-6 flex flex-col gap-y-4">
-                            <Button className="h-full w-full" variant="outline">
+                            <Button className="h-full w-full" variant="outline" onClick={() => handleSelect(1)}>
                                 <div>
-                                    {promptData[0].prompt}
+                                    {option1.text}
                                 </div>
                             </Button>
-                            <Button className="h-full w-full" variant="outline">
+                            <Button className="h-full w-full" variant="outline" onClick={() => handleSelect(2)}>
                                 <div>
-                                    {promptData[1].prompt}
+                                    {option2.text}
                                 </div>
                             </Button>
                         </div>
                         <div className="p-6 flex flex-col gap-y-2">
-                            <Label className="text-xs text-gray-600">TUNED PROMPT</Label>
+                            <Label className="text-xs text-primary/50">TUNED PROMPT</Label>
                             <p>{promptData[0].prompt}</p>
                         </div>
                     </div>
-                    <div className="flex flex-col w-full h-full border">
+                    <div className="flex flex-col w-full h-full border-l">
                         <div className="flex w-full border-b">
                             <div className="flex w-full justify-between">
                                 <div className="p-6">
-                                    <Label className="text-gray-600">PREVIEW</Label>
+                                    <Label className="text-primary/50">PREVIEW</Label>
                                 </div>
                                 <div className="p-4">
                                     <Select>
@@ -173,7 +220,7 @@ export default function Tool () {
                                     onKeyDown={(e) => {
                                         if (e.key === 'Enter' && !e.shiftKey) {
                                             e.preventDefault();
-                                            submitChatPrompt(e, "desktop");
+                                            submitChatPrompt(e);
                                         }
                                     }}
                                 />
@@ -182,7 +229,7 @@ export default function Tool () {
                                     onClick={(e) => submitChatPrompt(e, "desktop")}
                                     className="absolute md:top-1 top-0 right-2.5 rounded-lg p-2"
                                 >
-                                    {loading ? <ThreeDots fill="#1e293b" speed={0.75} className='rounded-r-md h-5 w-5' /> :
+                                    {chatLoading ? <ThreeDots fill="#1e293b" speed={0.75} className='rounded-r-md h-5 w-5' /> :
                                         <PaperPlaneIcon className='h-5 w-5 hover:text-gray-500' />}
                                     <span className="sr-only">Send message</span>
                                 </button>
